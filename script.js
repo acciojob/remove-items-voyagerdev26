a0160a41af2344c2439d6c3b543660d0b4dafb79
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-01-30 10:20:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let select=document.body.querySelector("#colorSelect");
+select.addEventListener("change",optionVal);
+let option;
+function optionVal(e){
+	option=this.options[this.selectedIndex];
+}
+
+let input=document.body.querySelector("input");
+input.addEventListener("click",removeOpt);
+function removeOpt(e){
+	option.remove();
+}
