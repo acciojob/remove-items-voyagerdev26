36c31bb5b3f2be3a11a55b6fd1e1eebf2f37d144
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-03-15 15:50:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 //your JS code here. If required.
-let select=document.querySelector("#colorSelect");
-select.addEventListener("change",optionVal);
-let option;
-function optionVal(e){
-	option=this.options[this.selectedIndex];
-}
+// let select=document.querySelector("#colorSelect");
+// select.addEventListener("change",optionVal);
+// let option;
+// function optionVal(e){
+// 	option=this.options[this.selectedIndex];
+// }
 
-let input=document.querySelector("input");
-input.addEventListener("click",removeOpt);
-function removeOpt(e){
+// let input=document.querySelector("input");
+// input.addEventListener("click",removeOpt);
+// function removeOpt(e){
+// 	option.remove();
+// }
+
+// 2nd way
+let input= document.querySelector("input");
+let selector= document.getElementById("colorSelect");
+input.addEventListener("click",(e)=>{
+	let option=selector.value;
 	option.remove();
-}
+})
